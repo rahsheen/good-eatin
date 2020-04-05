d2d07632fb99d926fdfc2cb1b1a85c65b13274ca
Rahsheen Porter <rahsheen.porter@gmail.com>
2020-04-05 17:57:05 -0400

    More API updates

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,34 +1,21 @@
-import { Box, IconButton, useColorMode } from "@chakra-ui/core";
+import { Box } from "@chakra-ui/core";
 import "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useUser } from "../context/user-context";
-import firebase from "../services/firebase";
 import { Restaurant } from "./Restaurant";
+import { getRestaurants } from "../utils/api";
 
 const Dashboard = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
   const [restaurant, setRestaurant] = useState<
     firebase.firestore.DocumentData
   >();
   const user = useUser();
 
   useEffect(() => {
-    firebase
-      .firestore()
-      .collection("restaurants")
-      .doc(user!.uid)
-      .get()
-      .then(function (doc) {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          setRestaurant(doc.data());
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      })
-      .catch(function (error) {
-        console.log("Error getting document:", error);
+    getRestaurants(user!.uid)
+      .then(r => r && setRestaurant(r))
+      .catch(error => {
+        console.log("Error getting restaurant:", error);
       });
   }, [user]);
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,25 @@
 import firebase from "../services/firebase";
 
+const restaurantsRef = firebase.firestore().collection("restaurants");
+
 export function saveRestaurant(uid: string, values: object) {
-  return firebase.firestore().collection("restaurants").doc(uid).set(values);
+  if (!uid || !values) throw new Error("Must provide a uid and values");
+
+  return restaurantsRef.add({ ...values, uid }).then(console.log);
+}
+
+export function getRestaurants(uid: string) {
+  if (!uid) throw new Error("Must provide a uid");
+
+  return restaurantsRef
+    .where("uid", "==", uid)
+    .get()
+    .then(snapshot => {
+      const arr: any[] = [];
+      snapshot.forEach(doc => {
+        // doc.data() is never undefined for query doc snapshots
+        arr.push(doc.data());
+      });
+      return arr;
+    });
 }
